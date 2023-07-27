17391f72e665465616b19c1612900e5b7e6f4214
yonekuramiki <yonekuramiki@yonekuramikinoMacBook-Pro.local>
2023-07-27 16:56:37 +0900

    Authrize cors

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
 const auth = require('./routes/auth');
 const post = require('./routes/post');
+
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
+
 
 app.use(express.json());
 app.use("/auth", auth)
